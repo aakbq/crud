acf6052cc52042fcd8f2f2ce843743c66d4ab72f
aakbq <97110798+aakbq@users.noreply.github.com>
2022-05-13 17:47:47 +0600

    test

--- server.js
+++ server.js
@@ -38,7 +38,6 @@ let port = process.env.PORT;
 if (port == null || port == "") {
     port = 8000;
 }
-app.listen(port);
 
 app.listen(port,() =>
     console.log(`App listening at http://localhost:${port}/landing`)
